8d0ed97c0ae7dc133f783ba0282446f0903cee93
mvn-rvn <xismiranims@hotmail.com>
2025-08-01 00:17:47 -0400

    d

--- Assets/Scripts/Cam Scripts/CamMovement.cs
+++ Assets/Scripts/Cam Scripts/CamMovement.cs
@@ -4,14 +4,22 @@ public class CamMovement : MonoBehaviour
 {
     public GameObject target;
     public float smoothing_time = 0.3f;
+
     Vector2 movement_velocity = Vector2.zero;
     float size_velocity = 0f;
+    Vector2 scale_velocity = Vector2.zero;
 
     float original_z;
     float original_size;
+    Vector2 original_scale;
 
-    GameObject staging_area;
+    public GameObject staging_area;
     Camera cam;
+
+    public GameObject left_bounds_object;
+    public GameObject right_bounds_object;
+    BoxCollider2D left_bounds;
+    BoxCollider2D right_bounds;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -21,6 +29,13 @@ public class CamMovement : MonoBehaviour
 
         cam = gameObject.GetComponent<Camera>();
         original_size = cam.orthographicSize;
+
+        original_scale = transform.localScale;
+
+        left_bounds = left_bounds_object.GetComponent<BoxCollider2D>();
+        right_bounds = right_bounds_object.GetComponent<BoxCollider2D>();
+        left_bounds.enabled = false;
+        right_bounds.enabled = false;
     }
 
     // Update is called once per frame
@@ -37,11 +52,36 @@ public class CamMovement : MonoBehaviour
             transform.position = new Vector3(transform.position.x, transform.position.y, original_z);
 
             cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, original_size, ref size_velocity, smoothing_time);
+
+            transform.localScale = Vector2.SmoothDamp(transform.localScale, original_scale, ref scale_velocity, smoothing_time);
         }
-        
+
         else
         {
+            left_bounds.enabled = true;
+            right_bounds.enabled = true;
 
+            transform.position = Vector2.SmoothDamp(transform.position, staging_area.transform.position, ref movement_velocity, smoothing_time);
+            transform.position = new Vector3(transform.position.x, transform.position.y, original_z);
+
+            float adjusted_size = staging_area.transform.localScale.x / 3.55f;
+
+            cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, adjusted_size, ref size_velocity, smoothing_time);
+
+            Vector2 adjusted_scale = new Vector2(
+                adjusted_size,
+                adjusted_size
+            );
+
+            transform.localScale = Vector2.SmoothDamp(transform.localScale, adjusted_scale, ref scale_velocity, smoothing_time);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (LayerMask.LayerToName(col.gameObject.layer) == "Cam Staging Area")
+        {
+            staging_area = col.gameObject;
         }
     }
 }
